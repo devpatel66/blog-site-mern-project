a43d52dcd6844c3edb4c8d80dab927e5cc7000a1
devpatel66 <devpatel16@outlook.ingit config --global user.email devpatel16@outlook.ingit config --global user.email devpatel16@outlook.in>
2024-04-18 20:31:16 +0530

    created for generating access and refesh token and method for encypting the password

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema, Types} from 'mongoose'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 
 const userSchema = Schema({
     fullname :{
@@ -32,5 +34,45 @@ const userSchema = Schema({
     }
 
 },{timestamps : true})
+
+userSchema.pre('save',async function(){
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password,10)
+    }
+})
+
+userSchema.methods.isPassword = async function(password){
+    const encryptedPassword = this.password
+    return await bcrypt.compare(password,encryptedPassword)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            fullname : this.fullname,
+            username : this.username
+        },
+        process.env.ACCESS_TOKEN_KEY,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY,
+            algorithm : 'RS256'
+        }
+    )
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_KEY,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY,
+            algorithm : 'RS256'
+        }
+    )
+}
 
 export const User = mongoose.model("user",userSchema);
