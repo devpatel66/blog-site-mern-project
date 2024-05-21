2b745d3e2be9b52fedff5770845ce76d09ed1886
devpatel66 <devpatel16@outlook.ingit config --global user.email devpatel16@outlook.ingit config --global user.email devpatel16@outlook.in>
2024-05-21 17:53:18 +0530

    fixed some issues

--- Frontend/src/tags/input.jsx
+++ Frontend/src/tags/input.jsx
@@ -2,7 +2,7 @@
 
 function Input({id,name,className="",type="text",value=null,disable=false}) {
   return (
-    <input id={id} name={name} type={type} required className={`block w-full rounded-md border-0 text-gray-900 px-4 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${className}`} value={value} disabled={disable}/>
+    <input id={id} name={name} type={type} required className={`block w-full rounded-md border-0 text-gray-900 px-4 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${className}`} value={value} readOnly={disable}/>
   )
 }
 
